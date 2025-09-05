cb4bee77e8f28aa097bbff821571e129d38f88dd
Bernardo Enock <91747672+bernardoenock@users.noreply.github.com>
2025-09-05 15:45:56 -0300

    Update Produto.cs add new fields

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -1,10 +1,19 @@
-﻿namespace TesteASPNET.Domain.Entities
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TesteASPNET.Domain.Entities
 {
     public class Produto
     {
         public int Id { get; set; }
+
         public string Nome { get; set; }
+
         public decimal Preco { get; set; }
+
         public int Estoque { get; set; }
+
+        public DateTime? DataAtualizacao { get; set; }
+        public DateTime? DataDelecao { get; set; }
     }
 }
